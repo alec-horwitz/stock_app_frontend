d1a4ae5e1aaa7ffa92c35b0bfbd878c69d51cbce
Alec <Alec@Christinas-MacBook-Air.local>
2019-04-03 16:59:51 -0400

    added styling and navigation to site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import Menu from './components/Menu';
+import Graph from './components/Graph';
+import Settings from './components/Settings';
+import Suggestions from './components/Suggestions';
 import './App.css';
 
 class App extends Component {
+  state={
+    page: "Menu",
+    parent: null
+  }
+  handleOnClick = (page, parent) => {
+    this.setState({page, parent})
+  }
   render() {
     return (
       <div className="App">
-        place holder text
+        {this.state.page === "Menu" ? <Menu handleOnClick={this.handleOnClick} /> : null}
+        {this.state.page === "Graph" ? <Graph handleOnClick={this.handleOnClick} /> : null}
+        {this.state.page === "Settings" ? <Settings parent={this.state.parent} handleOnClick={this.handleOnClick} /> : null}
+        {this.state.page === "Suggestions" ? <Suggestions handleOnClick={this.handleOnClick} /> : null}
+
       </div>
     );
   }
